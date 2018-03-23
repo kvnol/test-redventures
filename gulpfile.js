afd26b3376e599aaacfa0c57f0199d9f8e85850c
Kevin Oliveira <me@kevinoliveira.com.br>
2018-03-22 23:49:56 -0300

    Create gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
 const files = {
+  views: [
+    'src/views/*.pug'
+  ],
   css: [
     'src/assets/sass/main.sass'
   ],
@@ -13,4 +16,23 @@ const files = {
   ]
 };
 
-gulp.task('default', ['css', 'js']);
+gulp.task('views', () => {
+  return gulp.src(files.views)
+    .pipe(pug())
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('css', () => {
+  return gulp.src(files.css)
+    .pipe(sass())
+    .pipe(gulp.dest('app/assets/css'));
+});
+
+gulp.task('js', () => {
+  return gulp.src(files.js)
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/assets/js'));
+});
+
+gulp.task('default', ['views', 'css', 'js']);
