8938a05dc9741781d3e8ea533289964b0b606141
Kevin Oliveira <me@kevinoliveira.com.br>
2018-03-24 21:13:30 -0300

    Add weekdaysShort

--- src/assets/js/charlotte.date.js
+++ src/assets/js/charlotte.date.js
@@ -3,15 +3,21 @@
 
   const field = doc.querySelector('[data-js="date-input"]');
   const calendar = doc.querySelector('[data-js="dates-calendar"]');
+  const i18n = {
+    months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+    weekdays: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+    weekdaysShort: ['S','M','T','W','T','F','S']
+  };
   const picker = new Pikaday({
     field: field,
     container: calendar,
     bound: false,
     format: 'MMMM D, YYYY',
-    firstDay: 1,
-    minDate: new Date(2000, 0, 1),
-    maxDate: new Date(2020, 12, 31),
-    yearRange: [2000,2020]
+    minDate: new Date(2017, 0, 1),
+    maxDate: new Date(2027, 12, 31),
+    yearRange: [2017,2027],
+    i18n: i18n,
+    firstDay: 0
   });
 
   picker.setMoment(moment().dayOfYear(366));
